6e81e8ee48765920c0d89ef97f38792554ce677b
Flyer4109 <isao@rayamos.com>
2018-12-22 13:48:54 +0000

    Add bar charts

--- data_exploration.py
+++ data_exploration.py
@@ -1,5 +1,6 @@
 import cv2.cv2 as cv2
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # function to explore the data and print useful information
@@ -14,14 +15,37 @@ def explore():
 
     print('\nMode id:', labels['Id'].mode()[0])
 
-    # shows the 16 most common labels
-    print('\nMost frequent ids:\n', labels['Id'].value_counts()[:16])
+    # shows the 20 most common labels
+    print('\nMost frequent ids:\n', labels['Id'].value_counts()[:20])
 
     # reads every image then gets their shape and creates a DataFrame of shapes
     image_shapes = pd.Series([cv2.imread('../data/train/' + im).shape for im in labels['Image']])
-    
-    # shows the 16 most common image shapes
-    print('\nMost frequent image shapes:\n', image_shapes.value_counts()[:16])
+
+    # shows the 20 most common image shapes
+    print('\nMost frequent image shapes:\n', image_shapes.value_counts()[:20])
+
+    # Series that stores the counts of each label in order
+    label_counts = labels['Id'].value_counts()
+
+    # Series that stores the counts of each image resolution in order
+    image_shape_counts = image_shapes.value_counts()
+
+    # useful bar charts to help visualise data
+    label_counts[1:20].plot(kind='bar', title='The 20 whales with the most images')
+    plt.ylabel('Count')
+    plt.show()
+
+    label_counts[-20:].plot(kind='bar', title='Sample of whales with only one image')
+    plt.ylabel('Count')
+    plt.show()
+
+    image_shape_counts[:20].plot(kind='bar', title='The 20 most common image resolutions')
+    plt.ylabel('Count')
+    plt.show()
+
+    image_shape_counts[-20:].plot(kind='bar', title='Sample of image resolutions with only one image')
+    plt.ylabel('Count')
+    plt.show()
 
 
 if __name__ == '__main__':
