8925e8eb71cd0288d397976e9490241a5c7e9d8b
Flyer4109 <isao@rayamos.com>
2018-12-22 13:52:14 +0000

    Restructure print statements

--- data_exploration.py
+++ data_exploration.py
@@ -9,26 +9,26 @@ def explore():
     labels = pd.read_csv('../data/train/train.csv')
 
     # helpful information of the data
+    print('\nDescription of labels:\n', labels.describe())
+
     print('\nNumber of images:', len(labels['Image']))
 
-    print('\nDescription:\n', labels.describe())
-
-    print('\nMode id:', labels['Id'].mode()[0])
-
-    # shows the 20 most common labels
-    print('\nMost frequent ids:\n', labels['Id'].value_counts()[:20])
+    print('\nMost frequent label:', labels['Id'].mode()[0])
 
     # reads every image then gets their shape and creates a DataFrame of shapes
     image_shapes = pd.Series([cv2.imread('../data/train/' + im).shape for im in labels['Image']])
-
-    # shows the 20 most common image shapes
-    print('\nMost frequent image shapes:\n', image_shapes.value_counts()[:20])
 
     # Series that stores the counts of each label in order
     label_counts = labels['Id'].value_counts()
 
     # Series that stores the counts of each image resolution in order
     image_shape_counts = image_shapes.value_counts()
+
+    # shows the 20 most common labels
+    print('\nMost frequent ids:\n', labels['Id'].value_counts()[:20])
+
+    # shows the 20 most common image shapes
+    print('\nMost frequent image shapes:\n', image_shapes.value_counts()[:20])
 
     # useful bar charts to help visualise data
     label_counts[1:20].plot(kind='bar', title='The 20 whales with the most images')
